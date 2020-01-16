e904b1034a3770cc55f09aa1bcd1a6395195fe18
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 19:50:55 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,2 +1,28 @@
-print('hello')
-p
+class Deck:
+  
+   def __init__(self):
+      self.cards = []
+      for suit in range(4):
+        for rank in range(1, 14)
+        card = Card(suit, rank)
+        self.cards.append(card)
+        
+        
+def __str__(self):
+    res = []
+    for card in self.cards:
+      res.append(str(card))
+    return '\n'.join(res)
+  
+deck = Deck()
+print(deck)
+Ace of clubs
+2 of clubs
+3 of clubs
+
+10 of Spades
+jack of Spades
+Queen of Spades
+King of Spades
+        
+ 
